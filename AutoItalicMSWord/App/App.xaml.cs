dafe2e76d98b8abcac5c9e51482ad26421b076ee
Иван Борисов <ivan.alo@mail.ru>
2021-05-28 20:53:48 +0300

    Add C# version of AutoItalic

---
+++ AutoItalicMSWord/App/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Office.Interop.Word;
+
+namespace AutoItalicMSWord.App
+{
+    public partial class AutoItalicApplication
+    {
+        public readonly Application WordApplication = new();
+
+        ~AutoItalicApplication()
+        {
+            foreach (Document document in WordApplication.Documents)
+            {
+                document.Close(WdSaveOptions.wdDoNotSaveChanges);
+            }
+            
+            WordApplication.Quit();
+        }
+    }
+}
---
+++ AutoItalicMSWord/Extensions/WordExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Office.Interop.Word;
+
+namespace AutoItalicMSWord.Extensions
+{
+    public static class WordExtensions
+    {
+        public static void CloseAllDocuments(this Application wordApplication)
+        {
+            foreach (Document document in wordApplication.Documents)
+            {
+                document.Close(WdSaveOptions.wdDoNotSaveChanges);
+            }
+        }
+    }
+}
